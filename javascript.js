64a3d2537e2397447d02c434668ca00955a04e9a
jeann26 <124705872+jeann26@users.noreply.github.com>
2023-02-16 10:38:11 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//alert("alert")
+
+/*$(".menu-area").click(function(){
+     if ($(".menu-area").hasClass("active")) {
+         $(".menu-area").removeClass ("active")
+     }else
+     $(".menu-area").addClass("active")
+    })*/
+
+$(".menu-area").click(function(){
+    $(".menu-area").addClass("active")
+
+})
